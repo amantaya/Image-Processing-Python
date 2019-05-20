bf1a3e9f505824e7b05184543677bfaeee70c248
Mark M. Meysenburg <mark.meysenburg@doane.edu>
2019-05-19 19:08:04 -0500

    prepped for 2019 workshop

--- meysenburg/npc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run the SEM nanoparticle connectedness program on every image
-
-# then, execute the program on all the trail images
-for f in ca35-*.tif
-do
-	python NPConnect.py $f nd
-done
