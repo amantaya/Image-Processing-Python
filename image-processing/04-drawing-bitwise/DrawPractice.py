2d32346deadfa9f984d67efb74a921211f1532bb
Mark Meysenburg <mark.meysenburg@doane.local>
2020-02-20 20:37:14 -0600

    updated 04-drawing-bitwise/DrawPractice.py for skimage

--- image-processing/04-drawing-bitwise/DrawPractice.py
+++ image-processing/04-drawing-bitwise/DrawPractice.py
@@ -1,15 +1,16 @@
-'''
- * Program to practice with OpenCV drawing methods.
-'''
-import cv2
+"""
+ * Program to practice with skimage drawing methods.
+"""
+import random
 import numpy as np
+import skimage
+from skimage.viewer import ImageViewer
 
 # create the black canvas
-image = np.zeros(shape = (600, 800, 3), dtype = "uint8")
+image = np.zeros(shape=(600, 800, 3), dtype="uint8")
 
 # WRITE YOUR CODE TO DRAW ON THE IMAGE HERE
 
 # display the results
-cv2.namedWindow(winname = "image", flags = cv2.WINDOW_NORMAL)
-cv2.imshow(winname = "image", image)
-cv2.waitKey(delay = 0)
+viewer = ImageViewer(image)
+viewer.show()
